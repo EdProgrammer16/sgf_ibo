56133ac8740fac458544e2f16714a9864bb62fc0
EdProgrammer16 <79577511+EdProgrammer16@users.noreply.github.com>
2026-01-29 05:41:17 +0100

    Update parcelaRoutes.js

--- src/routes/parcelaRoutes.js
+++ src/routes/parcelaRoutes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const parcelaController = require("../controllers/parcelaController");
 
-console.log("parcelaController:", parcelaController);
-
 router.get("/", parcelaController.getAll);
+router.get("/:id", parcelaController.getById);
 
 module.exports = router;
