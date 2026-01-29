6bd9da8d5949e1dc8acfca65e149e460bfd0f715
EdProgrammer16 <79577511+EdProgrammer16@users.noreply.github.com>
2026-01-29 05:41:55 +0100

    Update parcelaController.js

--- src/controllers/parcelaController.js
+++ src/controllers/parcelaController.js
@@ -9,3 +9,10 @@ exports.getAll = async (req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(JSON.stringify(result));
 };
+
+exports.getById = async (req, res) => {
+  const { id } = req.params;
+  const result = await sql`SELECT * FROM parcela_de_terra WHERE id_parcela = ${id};`;
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(result));
+};
